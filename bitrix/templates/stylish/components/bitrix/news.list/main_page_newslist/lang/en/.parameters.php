50465d98fdb30e509a20f7d8e60d18a222f0f582
Алексей <a@coffeediz.ru>
2014-03-10 16:50:44 +0400

    Урок 8
    Список новостей на главной
    
    Простой и короткий урок на закрепление материала по работе с компонентом
    списка новостей инфоблока.

---
+++ bitrix/templates/stylish/components/bitrix/news.list/main_page_newslist/lang/en/.parameters.php
@@ -0,0 +1,7 @@
+<?
+$MESS ['T_IBLOCK_DESC_NEWS_DATE'] = "Display element date";
+$MESS ['T_IBLOCK_DESC_NEWS_NAME'] = "Display element title";
+$MESS ['T_IBLOCK_DESC_NEWS_PICTURE'] = "Display element preview picture";
+$MESS ['T_IBLOCK_DESC_NEWS_TEXT'] = "Display element preview text";
+$MESS ['NEWSLIST_TITLE'] = "Title of News List";
+?>
--- index.php
+++ index.php
@@ -42,24 +42,60 @@ $APPLICATION->SetTitle("Главная страница");
             <a href="#" class="link">More Info</a>
         </div>    
         <div class="span4">
-        	<h4 class="indent-2">Latest News:</h4>
-            <ul class="list-news">
-            	<li>
-                	<a href="#" class="btn btn_">Apr 21, 2012</a>
-                    <p class="text-info">Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta.</p>
-                    Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. <a href="#" class="underline">>></a>
-                </li>
-                <li>
-                	<a href="#" class="btn btn_">Apr 21, 2012</a>
-                    <p class="text-info">Aenean nonummy hendrerit mauris</p>
-                    Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes.  <a href="#" class="underline">>></a>
-                </li>
-                <li>
-                	<a href="#" class="btn btn_">Apr 21, 2012</a>
-                    <p class="text-info">Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturien.</p>
-                    Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes.  <a href="#" class="underline">>></a>
-                </li>
-            </ul>
+<?$APPLICATION->IncludeComponent("bitrix:news.list", "main_page_newslist", array(
+	"IBLOCK_TYPE" => "-",
+	"IBLOCK_ID" => "NEWS",
+	"NEWS_COUNT" => "20",
+	"SORT_BY1" => "ACTIVE_FROM",
+	"SORT_ORDER1" => "DESC",
+	"SORT_BY2" => "SORT",
+	"SORT_ORDER2" => "ASC",
+	"FILTER_NAME" => "",
+	"FIELD_CODE" => array(
+		0 => "",
+		1 => "",
+	),
+	"PROPERTY_CODE" => array(
+		0 => "",
+		1 => "",
+	),
+	"CHECK_DATES" => "Y",
+	"DETAIL_URL" => "",
+	"AJAX_MODE" => "N",
+	"AJAX_OPTION_JUMP" => "N",
+	"AJAX_OPTION_STYLE" => "N",
+	"AJAX_OPTION_HISTORY" => "N",
+	"CACHE_TYPE" => "A",
+	"CACHE_TIME" => "36000000",
+	"CACHE_FILTER" => "N",
+	"CACHE_GROUPS" => "Y",
+	"PREVIEW_TRUNCATE_LEN" => "",
+	"ACTIVE_DATE_FORMAT" => "f j, Y",
+	"SET_TITLE" => "N",
+	"SET_STATUS_404" => "N",
+	"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+	"ADD_SECTIONS_CHAIN" => "N",
+	"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+	"PARENT_SECTION" => "",
+	"PARENT_SECTION_CODE" => "",
+	"INCLUDE_SUBSECTIONS" => "Y",
+	"DISPLAY_TOP_PAGER" => "N",
+	"DISPLAY_BOTTOM_PAGER" => "N",
+	"PAGER_TITLE" => "Новости",
+	"PAGER_SHOW_ALWAYS" => "N",
+	"PAGER_TEMPLATE" => "",
+	"PAGER_DESC_NUMBERING" => "N",
+	"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+	"PAGER_SHOW_ALL" => "N",
+	"DISPLAY_DATE" => "Y",
+	"DISPLAY_NAME" => "Y",
+	"DISPLAY_PICTURE" => "Y",
+	"DISPLAY_PREVIEW_TEXT" => "Y",
+	"NEWSLIST_TITLE" => "Новости",
+	"AJAX_OPTION_ADDITIONAL" => ""
+	),
+	false
+);?>
             <h4 class="indent-1">testimonials:</h4>
 			<p class="p1">Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada.”</p>
             <span class="clr"><strong>Mark Priston</strong>, <a href="#" class="link-2">demolink.org</a></span>
